b6f40c765ec2620e93dc58d33ecb76161531d876
AndersonSchinemann <anderschinemann@gmail.com>
2019-04-08 17:37:17 -0300

    inicio da implementação

---
+++ src/tabuleiro/Tabuleiro.java
@@ -0,0 +1,42 @@
+package tabuleiro;
+
+public class Tabuleiro {
+	private int linhas;
+	private int colunas;
+	private Peca[][] peca;
+
+	public Tabuleiro(int linhas, int colunas) {
+		this.linhas = linhas;
+		this.colunas = colunas;
+		peca = new Peca[linhas][colunas];
+		
+	}
+
+	public int getLinhas() {
+		return linhas;
+	}
+
+	public void setLinhas(int linhas) {
+		this.linhas = linhas;
+	}
+
+	public int getColunas() {
+		return colunas;
+	}
+
+	public void setColunas(int colunas) {
+		this.colunas = colunas;
+	}
+
+	public Peca[][] getPeca() {
+		return peca;
+	}
+
+	public void setPeca(Peca[][] peca) {
+		this.peca = peca;
+	}
+	
+	
+	
+
+}
